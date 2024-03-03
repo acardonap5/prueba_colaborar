aa1989d9dfe9bdcb187b94a5aa2e68f825f20c9d
carpaiz <32883274+carpaiz@users.noreply.github.com>
2024-03-03 09:13:59 -0600

    agrear clase

---
+++ AACP.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+class AACCP{
+	// agregar comentario
+	private : string nombres,apellidos,direccion;
+	
+	void crear();
+	void leer();
+	void actualizar();
+	void borrar();
+};
